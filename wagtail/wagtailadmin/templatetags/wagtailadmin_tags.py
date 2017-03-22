72b411b60fc9313d43a2a604bdf3273216218029
Nick Hudkins <nick@nickhudkins.com>
2017-03-22 10:26:22 -0400

    :bug: respect defined page permissions
    The default Page model defines `permissions_for_user` which returns what was being automatically created here. If we instead call the method defined on the page instance, we ensure we respect permissions defined in subclasses of the Page model.

--- wagtail/wagtailadmin/templatetags/wagtailadmin_tags.py
+++ wagtail/wagtailadmin/templatetags/wagtailadmin_tags.py
@@ -131,10 +131,10 @@ def page_permissions(context, page):
     Sets the variable 'page_perms' to a PagePermissionTester object that can be queried to find out
     what actions the current logged-in user can perform on the given page.
     """
-    # Create a UserPagePermissionsProxy object to represent the user's global permissions, and
+    # Use a Page's `permissions_for_user` method to represent the user's global permissions, and
     # cache it in the context for the duration of the page request, if one does not exist already
     if 'user_page_permissions' not in context:
-        context['user_page_permissions'] = UserPagePermissionsProxy(context['request'].user)
+        return page.permissions_for_user(context['request'].user)
 
     # Now retrieve a PagePermissionTester from it, specific to the given page
     return context['user_page_permissions'].for_page(page)
